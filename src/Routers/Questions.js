5b7242872a0a0c8f7ad5a4a0a5b8154fb8f3bfeb
Oscar <oscar.ruvalcaba18@tectijuana.edu.mx>
2021-10-23 17:55:49 -0700

    Prueba

--- src/Routers/Questions.js
+++ src/Routers/Questions.js
@@ -1,39 +1,32 @@
-import React from 'react';
-import './Questions.css';
+import React from "react";
+import "./Questions.css";
 
 const Questions = () => {
-    return (
-        <div className="questionsContainer">
-            <div className="questionsTitle">
-                <h1>Seccion de Preguntas Frecuentes</h1>
-            </div>
-            <div className="answersContainer">
-                <div>
-                    <p>
-                        <strong>
-                            Preguntas y respuesta
-                        </strong> 
-                    </p>
-                </div>
-                <div>
-                    <p>  
-                        <strong>
-                            Preguntas y respuesta
-                        </strong>
-                        
-                    </p>
-                </div>
-                <div>
-                    <p> 
-                        <strong>
-                          Preguntas y respuesta  
-                        </strong>
-                    </p>
-                </div>
-            </div>
-           
+  return (
+    <div className="questionsContainer">
+      <div className="questionsTitle">
+        <h1>Seccion de Preguntas Frecuentes</h1>
+      </div>
+      <div className="answersContainer">
+        <div>
+          <p>
+            <h1>Hola</h1>
+            <strong>Preguntas y respuesta</strong>
+          </p>
         </div>
-    )
-}
+        <div>
+          <p>
+            <strong>Preguntas y respuesta</strong>
+          </p>
+        </div>
+        <div>
+          <p>
+            <strong>Preguntas y respuesta</strong>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Questions;
